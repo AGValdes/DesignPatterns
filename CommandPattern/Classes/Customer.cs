458937fe648bdb1b06e480f38202c74d1e12634a
ameilia valdes <valdes.ameilia@gmail.com>
2021-09-13 13:31:19 -0700

    Added menuitem class and customer(invoker) class

---
+++ CommandPattern/Classes/Customer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Classes
+{
+  /// <summary>
+  /// The Invoker class
+  /// </summary>
+  public class Customer
+  {
+    private OrderCommand _orderCommand;
+    private MenuItem _menuItem;
+    private FastFoodOrder _order;
+
+    public Customer()
+    {
+      _order = new FastFoodOrder();
+    }
+
+    public void SetCommand(int commandOption)
+    {
+      _orderCommand = new CommandFactory().GetCommand(commandOption);
+    }
+
+    public void SetMenuItem(MenuItem item)
+    {
+      _menuItem = item;
+    }
+
+    public void ExecuteCommand()
+    {
+      _order.ExecuteCommand(_orderCommand, _menuItem);
+    }
+
+    public void ShowCurrentOrder()
+    {
+      _order.ShowCurrentItems();
+    }
+  }
+
+  public class CommandFactory
+  {
+    //Factory method
+    public OrderCommand GetCommand(int commandOption)
+    {
+      switch (commandOption)
+      {
+        case 1:
+          return new AddCommand();
+        case 2:
+          return new ModifyCommand();
+        case 3:
+          return new RemoveCommand();
+        default:
+          return new AddCommand();
+      }
+    }
+  }
+}
